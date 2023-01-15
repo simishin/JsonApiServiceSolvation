03fc225532227368d7407b2016fa9e3d5149f7f7
“simishin” <ms67i@yandex.ru>
2023-01-15 15:19:41 +0700

    Все кроме группового пуска.

--- src/test/java/org/top/jsonapiservicesolvation/logic/ActionImplTest.java
+++ src/test/java/org/top/jsonapiservicesolvation/logic/ActionImplTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.internal.matchers.Null;
 import org.top.jsonapiservicesolvation.entity.Answer;
 import org.top.jsonapiservicesolvation.entity.Demand;
 
@@ -21,6 +22,7 @@ public class ActionImplTest {
      * Ожидаемое исключение JUnit 5
      * https://habr.com/ru/post/591305/
      * Использование Assertions.assertThrows()
+     * используется для проверки более подробной информации о сгенерированном исключении
      */
     @Test
     void action() {
@@ -32,8 +34,18 @@ public class ActionImplTest {
             //Code under test
         });
         Assertions.assertEquals("Угол больше 2Пи", thrown.getMessage());
+ //       assertTrue(thrown.getMessage().contains("Угол больше 2Пи")); //альтернатива
     } //----------------------------------------------------------------------------------
+    @Test
+    void actionF() { //используется для проверки  только типа исключения.
+        Demand x = new Demand(3.0, 4.0, 390.0,"");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
 
+            Answer y = z.action(x);
+//            assertEquals(y.oppositeSide(),5);
+            //Code under test
+        });
+    } //----------------------------------------------------------------------------------
     /**
      * вариант с урока
      */
@@ -46,16 +58,34 @@ public class ActionImplTest {
         } catch (IllegalArgumentException e){ }
     }
 
+    /**
+     *
+     * @param y
+     * @param x1
+     * @param x2
+     * @param x3
+     * @param x4
+     */
     @ParameterizedTest
     @MethodSource("method1DataProvider")
-    public void method1(double y, double x1, double x2, double x3, String x4) {
-        assertEquals(y, z.action(new Demand(x1, x2, x3, x4)).oppositeSide(),0.0001);
+    public void method1(Double y, double x1, double x2, double x3, String x4) {
+        Demand x = new Demand(x1, x2, x3, x4);
+        if (y.isNaN()) {
+            System.out.println("IllegalArgumentException");
+            Assertions.assertThrows(IllegalArgumentException.class, () -> {
+                z.action(x);
+            });
+        }
+        else {
+            assertEquals(y, z.action(x).oppositeSide(),0.0001);
+        }
     }
-    static Stream<Arguments> method1DataProvider() { return Stream.of(
+    static Stream<Arguments> method1DataProvider() {
+        return Stream.of(
         arguments(3.0318, 10.0, 7.0, 3.0, ""),
-        arguments(5, 3.0, 4.0, 90.0, ""),
-        arguments(5, 3.0, 4.0, 90.0, "g"),
-        arguments(5.97944, 3.0, 4.0, 90.0, "r")
+        arguments(Double.NaN, 3.0, 4.0, 390, ""),
+        arguments(5.0, 3.0, 4.0, 90.0, ""),
+        arguments(5.915, 3.0, 4.0, 2.0, "r",0.1)
         );
     }
     //---------------------------------------------------------------
